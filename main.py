af507c1a41b647bb74859a5f4d4c71f0764b55d5
jisu-d <lim16111611@gmail.com>
2025-12-19 01:46:40 +0900

    라즈베리파이 종료하는 api추가

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from fastapi.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
+import uvicorn
+
 from overlay_qr_img import overlay_qr_on_image
 from print_img import printImgs
 
@@ -29,6 +31,11 @@ class PRINTOUT_INFO(BaseModel):
     printoutNum: int
     base64_data: str
 
+@app.post("/shutdown") # 라즈베리파이 종료 api
+def shutdown_raspberry_pi():
+    os.system("sudo shutdown -h now")
+    return {"message": "라즈베리파이가 종료됩니다..."}
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -38,11 +45,20 @@ def read_root(data: PRINTOUT_INFO, request: Request):
     combined_img_path, qr_img_path = overlay_qr_on_image(data.base64_data)
     printImgs(data.printoutNum, combined_img_path)
 
-    # Construct the full URL for the QR code image
-    # request.base_url returns the full scheme://host:port/
     image_url = f"{request.base_url}qrcodes/{qr_img_path}"
 
     return {
         'msg': 'success',
         'image_url': image_url
     }
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",  
+        host="0.0.0.0", 
+        port=8000, 
+        ssl_keyfile="./key.pem", 
+        ssl_certfile="./cert.pem", 
+        log_level="info",
+        reload=True             
+    )
